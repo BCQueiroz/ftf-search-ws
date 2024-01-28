efd4d7b7100a13b3d620af0f98845cc3fbb25592
bcqueiroz <bcqueiroz123@gmail.com>
2024-01-28 11:08:56 -0300

    feat: adicionando estrutura base para conexão com banco

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
+require('dotenv').config()
 const express = require('express')
-const port = process.env.PORT || 3001
+const postgres = require('postgres')
+
+const port = process.env.PORT || 3004
 const app = express()
 
+const postgressConnection = postgres(process.env.DB_CONNECTION, {})
+
+app.get('/', (req, res) => {
+    res.send("Hello mundo.")
+})
+
+app.get('/get-all-locals', async (req, res) => {
+    const allLocals = await postgressConnection`SELECT * FROM tb_local`
+    res.json(allLocals)
+})
+
 app.listen(port, () => {
-    console.log(`Iniciando servidor para teste, porta ${port}`)
+    console.log(`Iniciando servidor back-end, porta ${port}`)
 })
