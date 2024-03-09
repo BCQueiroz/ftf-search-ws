72627ebc0d5fbb349e8630c2804b43be719ec5e4
bcqueiroz <bcqueiroz123@gmail.com>
2024-03-09 16:25:30 -0300

    ajustes na config do vercel

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config()
 const express = require('express')
-const SearchService = require('./service/SearchService')
-const UserService = require('./service/UserService')
-const SavedLocalsService = require('./service/SavedLocalsService')
+const SearchService = require('../service/SearchService')
+const UserService = require('../service/UserService')
+const SavedLocalsService = require('../service/SavedLocalsService')
 
-const port = process.env.PORT || 3004
+const port = process.env.PORT || 3001
 const app = express()
 const searchService = new SearchService()
 const userService = new UserService()
@@ -17,3 +17,7 @@ app.use('/api/saved', savedLocalsService.routes)
 app.listen(port, () => {
     console.log(`Iniciando servidor back-end, porta ${port}`)
 })
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+module.exports = app
