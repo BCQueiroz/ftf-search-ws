860fcf163fb66ff74d7cee19a10a2c0aa2a484f7
bcqueiroz <bcqueiroz123@gmail.com>
2024-05-02 19:27:41 -0300

    test mudanças arquivo

--- utils/PgConnManager.js
+++ utils/PgConnManager.js
@@ -3,8 +3,17 @@ const { Pool } = pg
 const fs = require('fs')
 
 const pool = new Pool({ 
-    connectionString: process.env.DB_CONNECTION + '&sslrootcert=certificates/ca-certificate.crt' 
+    connectionString: process.env.DB_CONNECTION + '&sslrootcert=certificates/ca-certificate.crt',
 })
+
+/*
+const pool = new Pool({
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DB_PORT,
+}) */
 
 function executeQuery(text, params, callback) {
     return pool.query(text, params, callback)
