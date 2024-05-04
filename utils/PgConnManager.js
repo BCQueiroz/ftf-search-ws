697badc20aa1f1b9ec2d1712e971585bd82c8ba4
bcqueiroz <bcqueiroz123@gmail.com>
2024-05-03 21:11:24 -0300

    refactor: tirando código comentado

--- utils/PgConnManager.js
+++ utils/PgConnManager.js
@@ -1,19 +1,9 @@
 const pg = require('pg')
 const { Pool } = pg 
-const fs = require('fs')
 
 const pool = new Pool({ 
     connectionString: process.env.DB_CONNECTION
 })
-
-/*
-const pool = new Pool({
-    host: process.env.HOST,
-    user: process.env.USERNAME,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.DB_PORT,
-}) */
 
 function executeQuery(text, params, callback) {
     return pool.query(text, params, callback)
